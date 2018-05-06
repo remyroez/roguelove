7b99ca662b0d8ee24dfe29121797c4ec25d7a06f
れみろう <remyroez+github@gmail.com>
2018-05-06 20:53:36 +0900

    :sparkles: コンポーネント Collider を追加

---
+++ game/src/components/Collider.lua
@@ -0,0 +1,74 @@
+
+local lovetoys = require 'lovetoys.lovetoys'
+
+local Collider = lovetoys.Component.create('Collider')
+
+function Collider:initialize(layer, w, h)
+    self.width = w or 1
+    self.height = h or 1
+    self.layer = layer or 1
+    
+    self.collisions = {}
+    self.dirty = true
+end
+
+function Collider:reset(layer, w, h)
+    self.width = w or 1
+    self.height = h or 1
+    self.layer = layer or 1
+    
+    self.collisions = {}
+    self.dirty = true
+end
+
+function Collider:clear()
+    self.collisions = {}
+    self.dirty = true
+end
+
+function Collider:validatePosition(x, y)
+    local validate = false
+
+    if x < 1 then
+        -- error
+    elseif x > self.width then
+        -- error
+    elseif y < 1 then
+        -- error
+    elseif y > self.height then
+        -- error
+    else
+        validate = true
+    end
+
+    return validate
+end
+
+function Collider:setCollision(collision, x, y)
+    x = x or 1
+    y = y or 1
+    if not self:validatePosition(x, y) then
+        -- error
+    else
+        if self.collisions[x] == nil then
+            self.collisions[x] = {}
+        end
+        self.collisions[x][y] = collision
+        self.dirty = true
+    end
+end
+
+function Collider:getCollision(x, y)
+    x = x or 1
+    y = y or 1
+    if not self:validatePosition(x, y) then
+        -- error
+    elseif self.collisions[x] == nil then
+        -- error
+    else
+        return self.collisions[x][y]
+    end
+    return nil
+end
+
+return Collider
