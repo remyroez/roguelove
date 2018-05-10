7a75e3d41feca705bcb657bb4fb054df2f28106a
れみろう <remyroez+github@gmail.com>
2018-05-10 22:54:34 +0900

    :sparkles: コンポーネント Size を追加

---
+++ game/src/components/Size.lua
@@ -0,0 +1,23 @@
+
+local util = require 'util'
+
+local lovetoys = require 'lovetoys.lovetoys'
+
+local Component = lovetoys.Component.create('Size')
+
+function Component:initialize(w, h)
+    self.dirty = {}
+    self:reset(w, h)
+end
+
+function Component:flush()
+    util.fill(self.dirty)
+end
+
+function Component:reset(w, h)
+    self.width = w or 1
+    self.height = h or 1
+    self:flush()
+end
+
+return Component
