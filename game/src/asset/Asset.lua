b3ed57c61c032bc9b126c1fa4698bd5d3d6a9441
れみろう <remyroez+github@gmail.com>
2018-05-12 21:19:41 +0900

    :sparkles: アセット情報を保持するクラス Asset を追加

---
+++ game/src/asset/Asset.lua
@@ -0,0 +1,32 @@
+
+local util = require 'util'
+
+local class = require 'middleclass'
+
+local Asset = class('Asset')
+
+function Asset:initialize(json)
+    self.json = json or {}
+end
+
+function Asset:id()
+    return self.json.id
+end
+
+function Asset:title()
+    return self.json.name
+end
+
+function Asset:type()
+    return self.json.type
+end
+
+function Asset:isType(type)
+    return self:type() == type
+end
+
+function Asset:data(key)
+    return key == nil and self.json.data or self.json.data[key]
+end
+
+return Asset
