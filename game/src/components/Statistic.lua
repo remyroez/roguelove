afc59b2a2b078368cf1718a1097d415d5d45d080
れみろう <remyroez+github@gmail.com>
2018-05-18 00:00:11 +0900

    :sparkles: コンポーネント Statistic 追加

---
+++ game/src/components/Statistic.lua
@@ -0,0 +1,11 @@
+
+local util = require 'util'
+
+local lovetoys = require 'lovetoys.lovetoys'
+
+local Component = lovetoys.Component.create('Statistic')
+
+function Component:initialize()
+end
+
+return Component
