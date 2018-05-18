5b844898b15c520a30339bac70ca29e812a76792
れみろう <remyroez+github@gmail.com>
2018-05-18 23:44:45 +0900

    :sparkles: アトリビュートコンポーネントとアセットの仮組み込み

--- game/src/components/Attribute.lua
+++ game/src/components/Attribute.lua
@@ -5,15 +5,38 @@ local lovetoys = require 'lovetoys.lovetoys'
 
 local Component = lovetoys.Component.create('Attribute')
 
-function Component:initialize(stats)
-    self.stats = stats or {}
+function Component:initialize(asset)
+    self.asset = asset
+    self.stats = {
+        hp = self:maxHp()
+    }
+end
+
+function Component:hp()
+    return self.stats and self.stats.hp or 0
+end
+
+function Component:maxHp()
+    return self.asset and self.asset:maxHp() or 0
+end
+
+function Component:triggers()
+    return self.asset and self.asset:triggers() or {}
 end
 
 function Component:onCollision(me, other, action)
     local myAttribute, otherAttribute = util.gets('Attribute', me, other)
     print('on', action)
-    print('me', me.id, myAttribute and myAttribute.stats.hp or nil)
-    print('other', other.id, otherAttribute and otherAttribute.stats.hp or nil)
+    print('me',
+        me.id,
+        myAttribute:hp(),
+        myAttribute:maxHp()
+    )
+    print('other',
+        other.id,
+        otherAttribute:hp(),
+        otherAttribute:maxHp()
+    )
 end
 
 return Component
